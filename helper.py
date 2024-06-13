9976b8eee6af6e829964ace1d6813892cbd79b23
Moamen Ghareeb <61291595+moamen1358@users.noreply.github.com>
2024-06-13 13:12:20 +0300

    Update and rename mmm.py to helper.py

--- helper.py
+++ helper.py
@@ -3,7 +3,6 @@
 
 import tensorflow as tf
 
-
 # Create a function to import an image and resize it to be able to be used with our model
 def load_and_prep_image(filename, img_shape=224, scale=True):
   """
